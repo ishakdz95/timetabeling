5e888f49de4d7949070730234253ea97da7370d7
ishaq guermat <ishakdz1995@gmail.com>
2020-05-29 19:53:10 +0100

    not end

--- app/Http/Controllers/Admin/TimeTabelingController.php
+++ app/Http/Controllers/Admin/TimeTabelingController.php
@@ -42,6 +42,8 @@ class TimeTabelingController extends Controller
         $timeslotname=[];
         $i=0;
         $j=0;
+        $c=0;
+        $g=0;
 
         $r=0;
 
@@ -50,42 +52,70 @@ class TimeTabelingController extends Controller
             $list_rooms[$r]=$room->code;
             $r++;
         }
-
+        foreach ($courses as $cours){
+            $list_courses[$c]=$cours->name;
+            $c++;
+        }
+        foreach ($groups as $group){
+            $list_groups[$g]=$group->name;
+            $g++;
+        }
         foreach ($professors as $professor){
             $t=0;
-            $column='P:'.$professor->first_name.' ';
-
-            foreach ($days as $day){
-                foreach ($day->timeslots as $timeslot){
-                    $timeslotname[$t]=$timeslot->name;
-                    $t++;
-                }
-            }
-
-            foreach ($courses as $cours)
-            {
-
-                $booltime=1;
-                $boolroom=1;
-                $column=$column.'C: '.$cours->name.' ';
-                while ($booltime==1 && empty($timeslotname)!=true){
-                    $column=$column.$timeslotname[0].' ';
-                    $booltime=0;
-                    array_shift($timeslotname);
-                }
-                while ($boolroom==1 && empty($list_rooms)!=true){
-                    $column=$column.$list_rooms[0].' ';
-                    $boolroom=0;
-                    array_shift($list_rooms);
-                }
-                if (empty($list_rooms)){
-                    $r=0;
-                    foreach ($rooms as $room)
-                    {
-                        $list_rooms[$r]=$room->code;
-                        $r++;
+            $h=6;
+                    $column='P:'.$professor->first_name.' ';
+                    foreach ($days as $day){
+                         foreach ($day->timeslots as $timeslot){
+                            $timeslotname[$t]=$timeslot->name;
+                            $t++;
+                         }
                     }
-                }
+                    while ($h>0)
+                        {
+                            $h--;
+
+                           $booltime=1;
+                           $boolroom=1;
+                           $boolgroup=1;
+                           $column=$column.'C: '.$list_courses[0].' ';
+                           array_shift($list_courses);
+                           while ($booltime==1 && empty($timeslotname)!=true){
+                           $column=$column.$timeslotname[0].' ';
+                           $booltime=0;
+                           array_shift($timeslotname);
+                            }
+                            while ($boolroom==1 && empty($list_rooms)!=true){
+                           $column=$column.$list_rooms[0].' ';
+                           $boolroom=0;
+                           array_shift($list_rooms);
+                             }
+                            while ($boolgroup==1 && empty($list_groups)!=true){
+                                $column=$column.$list_groups[0].' ';
+                                $boolgroup=0;
+                                array_shift($list_rooms);
+                            }
+                          if ($c==$r || empty($list_rooms) ){
+                            $r=0;
+                            $c=0;
+                            foreach ($rooms as $room)
+                                {
+                                $list_rooms[$r]=$room->code;
+                                $r++;
+                                }
+                          }
+                            if ( empty($list_courses) ){
+                                $r=0;
+                                $c=0;
+                                foreach ($courses as $cours)
+                                {
+                                    $list_courses[$c]=$cours->name;
+                                    $c++;
+                                }
+                            }
+
+
+
+
             }
             $table[$i]=$column;
             $i++;
