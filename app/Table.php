a7844d0fccf9d310286460196957ad864843ded9
ishaq guermat <ishakdz1995@gmail.com>
2020-08-20 22:28:44 +0100

    afficher l'imploi du temp avec random,avec plusieur tableux

--- app/Table.php
+++ app/Table.php
@@ -12,6 +12,7 @@ class Table extends Model
         $days=Day::all();
         $timeslots=Timeslot::all();
         $timetabling=new TimeTabeling();
+
         $seances=$timetabling->cour_group();
         $timetabling->dettach_rooom_timeslots();
         $timetabling->dettach_professor_timeslots();
