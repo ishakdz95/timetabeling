49b1c2b30d4cf71750cbc48a0c5208f38c5bb154
farouk lilou <farouk.info@gmail.com>
2020-09-29 23:59:58 +0100

    remode add btn

--- resources/views/admin/sections/index.blade.php
+++ resources/views/admin/sections/index.blade.php
@@ -2,11 +2,11 @@
 {{-- the sidebare is rendred in the master.blade.php --}}
 @section('main-section')
     <div class="col-lg-8">
-
-    <addbutton-component :url="{{ json_encode(url('')) }}" :route="'/admin/sections/create'"></addbutton-component>
-            <sections-component :years="{{$years}}"> </sections-component>
+        <addbutton-component
+            :url="{{ json_encode(url('')) }}"
+            :route="'/admin/sections/create'"
+        ></addbutton-component>
+        <sections-component :years="{{$years}}"> </sections-component>
     </div>
-    <a href="{{route('admin.sections.create')}}" class="mb-2 mr-2 btn btn-success">Add New</a>
-
 
 @endsection
